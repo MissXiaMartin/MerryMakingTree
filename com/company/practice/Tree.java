714ae5f58c44317b6ff13862cfcd3d9fce04e6c2
MissXiaMartin <xmartin3@gmail.com>
2021-12-22 00:16:25 -0600

    initial commit

---
+++ com/company/practice/Tree.java
@@ -0,0 +1,19 @@
+package com.company.practice;
+
+public class Tree {
+
+	 public static void main(String[] args) {
+	//We’re going to introduce ourselves and print a tree to the screen. 
+	  System.out.println("Hey there, I’m a coder and I’m learning to code in Java! It's time to roll up my sleeves and create a small project.");
+	     System.out.println("  ....Stand by...  ");
+	        System.out.println(" ....................... "); 
+	        System.out.println("Now, Check out this cool tree I made below. Merry Christmas!");
+	    System.out.println("  *  ");
+	    System.out.println(" *** ");
+	   System.out.println(" ****  ");
+	    System.out.println(" *** ");
+	    System.out.println("  *  ");
+	    System.out.println("  *  ");
+	       System.out.println("Merry Christmas");
+}
+}
